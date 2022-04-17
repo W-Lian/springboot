29bac62f3aa08c7dbcd80180438b9b291cbfba49
michael941025 <1553279148@qq.com>
2022-04-17 23:36:22 +0800

    first commit

---
+++ springboot1/src/main/java/com/example/springboot1/control/GetTest.java
@@ -0,0 +1,41 @@
+package com.example.springboot1.control;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@RestController
+@RequestMapping("getTest")
+@Slf4j
+public class GetTest {
+    
+    @GetMapping("test1")
+    public String hello(@RequestParam String name){
+        return "hello "+ name;
+    }
+
+    @GetMapping("test2")
+    public void test2(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        HttpSession test2Session = req.getSession(true);
+        log.info("test2Session id"+test2Session.getId());
+        
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<br><a href="+ resp.encodeURL("test3")+">refresh</a>");
+    }
+
+    @GetMapping("test3")
+    public void test3(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        HttpSession test3Session = req.getSession(true);
+        log.info("test3Session id"+test3Session.getId());
+        
+    }
+}
---
+++ springboot1/src/main/java/com/example/springboot1/control/SessionTest.java
@@ -0,0 +1,28 @@
+package com.example.springboot1.control;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("sessionTest")
+@Slf4j
+public class SessionTest {
+    @Value("${server.port}")
+    Integer port;
+
+    @GetMapping("/set")
+    public String set(HttpSession session) {
+        session.setAttribute("user", "javaboy");
+        return String.valueOf(port);
+    }
+
+    @GetMapping("/get")
+    public String get(HttpSession session) {
+        return session.getAttribute("user") + ":" + port;
+    }
+}
